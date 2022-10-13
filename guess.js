a2ed684d353475156492aa335b75e4a14bbc28e8
AdiFischer <adifischer1@gmail.com>
2022-10-13 10:38:12 -0400

    plying

--- guess.js
+++ guess.js
@@ -6,3 +6,33 @@ if it's the right num say "congratulations' and exit".
 */
 
 import readline from "readline-sync"
+
+let userGuessedRight = false
+
+//while(true){
+    // guess a num bet 1-10 but with ceil-can be 10. with floor can be up tp 9
+let numberChosen = Math.ceil(Math.random() * 10)//ask for a guess of our num.
+//console.log(numberChosen)
+//}
+while(!userGuessedRight) {
+let guess = readline.question("What's your guess?")
+if (guess == numberChosen)// is it equal to
+{
+    console.log("congratulations")
+    userGuessedRight = true // get out
+}
+if (guess > numberChosen)
+{
+    console.log("Too High")
+    //userGuessedRight = true - if not comminted out, then chang to false
+}
+if (guess < numberChosen)
+{
+    console.log("Too Low")
+    //userGuessedRight = true
+}
+// console.log(guess)
+//}
+// while(!userGuessedRight){
+// console.log("What is your guess?")
+}
