37b2ede7794a1650d55591faa858120f4a3262f8
AdiFischer <adifischer1@gmail.com>
2022-10-13 09:35:34 -0400

    check

---
+++ guess.js
@@ -0,0 +1,8 @@
+/*
+create a program that guesses a num between 1-10 then asks us to guesss that num.
+if it hugher say "too high" and ask again.
+if lower say "too low" and ask again.
+if it's the right num say "congratulations' and exit".
+*/
+
+import readline from "readline-sync"
